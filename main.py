eb908253b98945582002c916280e4a83de9c6c15
Kathesan <kathesan.karunakaran@limayrac.fr>
2020-06-25 14:18:56 +0000

    ex5

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def exempleHello (msg):
 ###### exercice 01
 def makeDico_E6(nomFichier, sep):
   print("creation du dico")
-
   dico={}
 
   f = open(nomFichier,"r")
@@ -20,30 +19,93 @@ def makeDico_E6(nomFichier, sep):
   return dico
 
 ###### exercice 02
-def verifUrl_F4():
-  print("hello")
+def verifUrl_F4(url):
+  #print("hello")
+  format= url.count('.')
+
+  if format == 1 :
+    str = url.split('.')
+    x, y = str
+
+    if len(y) <= 3:
+      return True
+    else:
+      print("url mal formee")
+      return False
+
+  else:
+    print("url mal formee")
+    return False
+
 
 ###### exercice 03
-def getTLD_N5():
-  print("hello")
+def getTLD_N5(url):
+  #print("hello")
+  res = verifUrl_F4(url)
+
+  if res == True:
+    b = url.split('.')
+    return b[1]
+  else:
+    print("TLD mal formee")
+    return False
 
 ###### exercice 04
-def VerifLDT_J5():
-  print("hello")
+def VerifLDT_J5(tldOk, tld):
+  #print("hello")
+
+  for i in tldOk:
+    if tld == i:
+      return True
+
+  print("TLD absente")
+  return False
 
 ###### exercice 05
-def ipVerifFormat_I5(): 
-  print("hello")   
+def ipVerifFormat_I5(adresseIp): 
+  print("hello")
+
+  c = adresseIp.split('.')
+
+  for e in c:
+    if int(e) < 0 or int(e) > 255:
+      print("champ d'adresse incorrect")
+      return False
+
+  return True
+
+  if len(c) != 4:
+    print("nombre de champ incorrect")
+    return False
+
+
 
 ###### exercice 06
-#def makeTLD_A2():
+def makeTLD_A2(resolDns):
+  print("hello")
+  print (resolDns.keys())
+
+  for d in resolDns.keys():
+    str = d.split('.')
+    #x, y
 
 # Zone 2 ## zone pour les classes
 ###### exercice 07
-
-
+class serveurDns_V2 :
+  def __init__(self, resolDns):
+    self.resolDns = resolDns
+  
+    
 ###### exercice 08
+def resoltDns(self, url):
+  result = verifUrl_F4(url)
+  if result:
 
+    return True
+  else:
+
+    print("erreur de format de l'url")
+    return False
 
 ###### exercice 09
     
@@ -60,42 +122,53 @@ def main() :
 
 	###### exercice 01
   print("exercice 01 #######################")
-  resolDns = makeDico_E6("dns.txt", ",")
+  #resolDns = makeDico_E6("dns.txt", ",")
   #print(resolDns)
+
 	###### exercice 02
   print("exercice 02 #######################")
-
+  result = verifUrl_F4("Amazon.fr")
+  #print(result)
 
 	###### exercice 03
   print("exercice 03 #######################")
-
+  #result = VerifLDT_J5("Amazon.fr")
+  #print(result)
 
 	###### exercice 04
   print("exercice 04 #######################")
-
+  #result = VerifLDT_J5(["fr","com","net"],"fr")
+  #print(result)
 
 	###### exercice 05
   print("exercice 05 #######################")
-
+  result = ipVerifFormat_I5("178.236.6.242")
+  print(result)
 
 	###### exercice 06
   print("exercice 06 #######################")
+  #tldOk = makeTLD_A2(dico)
+  #print(result)
 
 	# Zone 4 ## zone pour les tests de la classe
 
 	###### exercice 07
   print("exercice 07 #######################")
 
-
+  #print(result)
 	###### exercice 08
   print("exercice 08 #######################")
 
-
+  #print(result)
 	###### exercice 09
   print("exercice 09 #######################")
-	
+
+  #print(result)
+
 	###### exercice 10
   print("exercice 10 #######################")
+
+  #print(result)
 
 if __name__=="__main__":
   print("main()")
